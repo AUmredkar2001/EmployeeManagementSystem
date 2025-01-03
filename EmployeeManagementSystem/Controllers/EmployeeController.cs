4985e52670c823845346f2d3e422cac7253c4256
Ankita Umredkar <Ankita.Umredkar@infogain.com>
2025-01-03 15:16:44 +0530

    Add project files.

---
+++ EmployeeManagementSystem/Controllers/EmployeeController.cs
@@ -0,0 +1,106 @@
+﻿using EmployeeManagementSystem.Data;
+using EmployeeManagementSystem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeDbContext _context;
+        public EmployeeController(EmployeeDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var employee = _context.Employees.ToList();
+            return View(employee);
+
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Add(employee);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var employee = _context.Employees.Find(id);
+            if (employee == null)
+                return NotFound();
+            return View(employee);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Update(employee);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var employee = _context.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var employee = _context.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var empoyee = _context.Employees.Find(id);
+            if(empoyee != null)
+            {
+                _context.Employees.Remove(empoyee);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+        [HttpGet]
+        public IActionResult GetEmployee()
+        {
+            var employee = _context.Employees.ToList();
+            return View(employee);
+        }
+
+    }
+}
---
+++ EmployeeManagementSystem/Controllers/LoginController.cs
@@ -0,0 +1,85 @@
+﻿using EmployeeManagementSystem.Data;
+using EmployeeManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly EmployeeDbContext _context;
+        private readonly IConfiguration _configuration;
+
+        public LoginController(EmployeeDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string username, string password)
+        {
+            var user = _context.Authentications.FirstOrDefault(u => u.UserName == username && u.Password == password);
+            if (user == null)
+            {
+                ViewBag.Message = "Invalid Credential.";
+                return View();
+            }
+            var token = GenerateToken(user);
+            TempData["Token"] = token;
+            return RedirectToAction("Index", "Employee");
+        }
+
+        private string GenerateToken(Authentication authentication)
+        {
+            var claim = new[]
+            {
+                new Claim(ClaimTypes.Name, authentication.UserName)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWTSettings:SecretKey"]));
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                claims: claim,
+                expires: DateTime.Now.AddHours(2),
+                signingCredentials: cred);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(Authentication authentication)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _context.Authentications.Add(authentication);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult HomePage()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return RedirectToAction("HomePage");
+        }
+    }
+}
